3eff26ed9cbaa16b9db9a3b6ada5c270f7794e8d
Wazzap <wazzap@Wazzaps-MacBook-Pro.local>
2026-03-31 23:47:47 +0400

    test practises

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
+
     rollupOptions: {
       input: {
         main: './index.html',
